c3c619a6af8909f835b7863acab99e8ba6aff774
sakura1116 <daisuke17260@gmail.com>
2020-03-26 01:47:07 +0900

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :companies, only: [:index] do
-    resources :articles, only: [:index]
+    member do
+        get :articles, to: 'companies#article_index'
+    end
   end
 end
